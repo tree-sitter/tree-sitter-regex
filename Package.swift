a71269a5854089ab587f3091d6ab98a1c4ca228e
Amaan Qureshi <amaanq12@gmail.com>
2024-05-05 20:50:02 -0400

    build: update bindings and manifests

---
+++ Package.swift
@@ -0,0 +1,46 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "TreeSitterRegex",
+    products: [
+        .library(name: "TreeSitterRegex", targets: ["TreeSitterRegex"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "TreeSitterRegex",
+                path: ".",
+                exclude: [
+                    "Cargo.toml",
+                    "Makefile",
+                    "binding.gyp",
+                    "bindings/c",
+                    "bindings/go",
+                    "bindings/node",
+                    "bindings/python",
+                    "bindings/rust",
+                    "prebuilds",
+                    "grammar.js",
+                    "package.json",
+                    "package-lock.json",
+                    "pyproject.toml",
+                    "setup.py",
+                    "test",
+                    "examples",
+                    ".editorconfig",
+                    ".github",
+                    ".gitignore",
+                    ".gitattributes",
+                    ".gitmodules",
+                ],
+                sources: [
+                    "src/parser.c",
+                ],
+                resources: [
+                    .copy("queries")
+                ],
+                publicHeadersPath: "bindings/swift",
+                cSettings: [.headerSearchPath("src")])
+    ],
+    cLanguageStandard: .c11
+)
